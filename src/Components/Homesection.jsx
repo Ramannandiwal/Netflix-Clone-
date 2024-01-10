68f38cae66b6f041f2d34c04bc75170930816f29
Ramannandiwal <raman.nandiwal@gmail.com>
2024-01-10 14:23:20 +0530

    last commit from myside

--- src/Components/Homesection.jsx
+++ src/Components/Homesection.jsx
@@ -4,7 +4,7 @@ import Heading from './Heading';
 
 function Homesection() {
   return (
-    <div className='w-screen h-screen border bg-cover  border-black' style={{ background: "url('src/assets/MainBanner.jpg')"}}>
+    <div className='w-screen h-screen border bg-cover  border-black' style={{ background: "url('	https://assets.nflxext.com/ffe/siteui/vlv3/b8dd515…trifectadaily-perspective_alpha_website_large.jpg')"}}>
         <div className=' w-full h-full' style={{background:"rgba(0,0,0,0.7)"}}>  
                    <Navbar/>
                    <Heading/>
